4b62bde491810f50659ca03788f26d6778eedbea
John-Baird <106564524+John-Baird@users.noreply.github.com>
2023-05-17 18:20:40 -0600

    Soft Wall Colisions Added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -18,9 +18,10 @@ export class GameComponent implements OnInit {
   playerSpeed: number = 2;
   canvas: any;
   player: any;
-  playerX: number = 50;
-  playerY: number = 50;
+  playerX: number = 100;
+  playerY: number = 100;
   CurrentKey: any = {up:0,down:0,left:0,right:0}
+  mapData: any;
   constructor(
     private afAuth: AngularFireAuth,
     private firestore: AngularFirestore,
@@ -33,12 +34,14 @@ export class GameComponent implements OnInit {
 
 
   private updateCanvas(): void {
-    fetch('./assets/Dungeon_Sprites/Dungeon 02.tsv')  //TODO Find out how to link the tsv file
+    fetch('./assets/Dungeon_Sprites/Dungeon 01.tsv')  //TODO Find out how to link the tsv file
     .then(response => response.text())
     .then(data => {
       console.log(data);
+      
       // Split the TSV data into rows
       const rows = data.split('\n');
+      
       // Get the number of rows and columns
       const numCols = rows[0].split('\t').length;
       const numRows = rows.length;
@@ -56,12 +59,15 @@ export class GameComponent implements OnInit {
       canvas.height = canvasHeight;
       // Get the 2D context
       const ctx = canvas.getContext('2d');
+
+      let map = []
       // Loop through each row and column
       for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numCols; j++) {
           // Get the value of the current cell
           const cellValue = rows[i].split('\t')[j];
           // Set the fill style to black if the cell is blank, or white if it has a value
+          map.push(cellValue)
           if (cellValue === '') {
             ctx.fillStyle = 'black';
           } else {
@@ -71,7 +77,7 @@ export class GameComponent implements OnInit {
           ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
         }
       }
-
+      this.mapData = map
 
       
     });
@@ -96,7 +102,7 @@ export class GameComponent implements OnInit {
     setInterval(() => {
       this.update();
       this.draw();
-      this.savePosition();
+      //this.savePosition();
     }, 16);
 
 
@@ -156,11 +162,44 @@ export class GameComponent implements OnInit {
 
   update(): void {
     // Update game logic here
+
+    //Canvas
+    const canvas = this.canvas
+    const ctx = canvas.getContext('2d')
+
+    const playerSize = 4
+    const newPlayerX = this.playerX + (this.CurrentKey.left*playerSize + this.CurrentKey.right*playerSize) * this.playerSpeed;
+    const newPlayerY = this.playerY - (this.CurrentKey.up*playerSize + this.CurrentKey.down*playerSize) * this.playerSpeed;
+
+
+    //Future Tile
+    function findTile(x,y,map){
+      const rowIndex = Math.floor(y / 21)-3;
+      const colIndex = Math.floor(x / 21);
+      const currentIndex = colIndex+21*rowIndex
+      const levelData = map
+      const tile = levelData[currentIndex]
+      return tile
+    }
     
-    //console.log(`Vertical: ${this.CurrentKey.up + this.CurrentKey.down} Horizontal: ${this.CurrentKey.left + this.CurrentKey.right}`)
-    //console.log(`X: ${this.playerX} Y: ${this.playerY}`)
-    this.playerX += (this.CurrentKey.left + this.CurrentKey.right) * this.playerSpeed
-    this.playerY -= (this.CurrentKey.up + this.CurrentKey.down) * this.playerSpeed
+    const tile = {x:'',y:''}
+    tile.x = findTile(newPlayerX,this.playerY,this.mapData)
+    tile.y = findTile(this.playerX,newPlayerY,this.mapData)
+
+
+
+    //Update Player
+    if ( tile.x === 'F' || tile.x === 'DL' || tile.x === 'DB' || tile.x === 'DT' || tile.x === 'DPL'){
+      this.playerX += (this.CurrentKey.left + this.CurrentKey.right) * this.playerSpeed
+      
+    }
+    if ( tile.y === 'F' || tile.y === 'DL' || tile.y === 'DB' || tile.y === 'DT' || tile.y === 'DPL'){
+      this.playerY -= (this.CurrentKey.up + this.CurrentKey.down) * this.playerSpeed
+      
+    }
+
+
+    
   }
 
 
